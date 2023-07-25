89114c6bcbd72989eea27a7116fcf45f800abdde
IrinaMa72 <morevaia72@gmail.com>
2023-07-25 23:21:03 +0500

    Initial commit

---
+++ src/test/java/com/skypro/hw211/Hw211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.hw211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw211ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
